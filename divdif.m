7569795c508a50855d6951fda4f432cad72ecdac
Tudor Stanila <tudorstanila@gmail.com>
2018-03-20 12:34:11 +0200

    Added lab4

--- divdif.m
+++ divdif.m
@@ -2,7 +2,7 @@ function T = divdif(x,f)
     %Input: x - the points xi
     % f - the values of f in in the points xi
     %Output: T - the result matrix
-    if size(x) == size(f)
+    if length(x) == length(f)
         n = size(x);
         T=zeros(n);
         T(:,1)=f;
@@ -10,5 +10,7 @@ function T = divdif(x,f)
            T(1:n-i+1,i) = diff(T(1:n-i+2,i-1))./(x(i:n)-x(1:n-i+1));
         end
         T=[x T];
+    else
+        T=[];
     end
 end
